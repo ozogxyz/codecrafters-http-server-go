d9038989f0f4f94bbf90394cb7ecb9ff501e69c2
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2024-07-11 15:59:52 +0300

    send http

--- app/server.go
+++ app/server.go
@@ -18,9 +18,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	resp := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(resp))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Response sent")
+
 }
