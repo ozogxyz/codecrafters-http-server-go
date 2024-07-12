dea3d563b2976d4dc6839b35412720ad6727b5d3
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2024-07-12 20:41:51 +0300

    add url read

--- app/server.go
+++ app/server.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+const (
+	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
+	OK        = "HTTP/1.1 200 OK\r\n\r\n"
+)
 
+func handleConnection(conn net.Conn) {
+	var buf []byte = make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection", err.Error())
+	}
+	if n > 1024 {
+		fmt.Println("Buffer overflow")
+		os.Exit(1)
+	}
+	url := strings.Fields(string(buf))[1]
+	if url == "/" {
+		conn.Write([]byte(OK))
+	} else {
+		conn.Write([]byte(NOT_FOUND))
+	}
+	conn.Close()
+}
+
+func main() {
 	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -24,12 +45,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	resp := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err = conn.Write([]byte(resp))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-		os.Exit(1)
-	}
-	fmt.Println("Response sent")
-
+	handleConnection(conn)
 }
